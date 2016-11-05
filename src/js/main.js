4163e6cab80efef1496c40e78b6aebebfd465285
amgmetallica <gareraanthony@gmail.com>
2016-11-05 14:54:41 -0400

    Merge remote-tracking branch 'origin/master' into front-end
    # Conflicts:
    #	src/index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,8 @@
+function getCompanyName() {
 
-$('img[data-default-src]').each(function(){
-    var defaultSrc = $(this).data('default-src');
-    $(this).on('error', function(){
-        $(this).attr({src: defaultSrc});
-    });
-});
-//TODO Fix this so that Company Logo's work.
-function getCompanyName(form) {
-    var cName = form.action;
-    $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + "https://logo.clearbit.com/" + cName + ".com");
-    console.log(cName);
+
+    if(event.keyCode == 13) {
+        var input = document.getElementById("search").value;
+        $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + input + ".com");
+    }
 }
-
-// $("#search").keyup(function(event){
-//     if(event.keyCode == 13){
-//         $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + "https://logo.clearbit.com/" + domain + ".com");
-//         return false;
-//     } else {
-//         return true;
-//     }
-// });
-
-
-
-// function getCompanyLogo(e) {
-//     var domain = document.getElementById("search");
-//     fetch("https://logo.clearbit.com/" + domain)
-//         .then(function (response) {
-//             console.log(response);
-//             return response;
-//
-//         });
-//
-//     $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + domain + ".com")
-//
-// }

ab4e1a628645faada2f74798126eab84394097fb
amgmetallica <gareraanthony@gmail.com>
2016-11-05 16:21:30 -0400

    Added Reset on Search

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ function getCompanyName() {
 
 
     if(event.keyCode == 13) {
+        $("#companyLogoImg").attr('src', 'https://logo.clearbit.com/');
         var input = document.getElementById("search").value;
         $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + input + ".com");
     }

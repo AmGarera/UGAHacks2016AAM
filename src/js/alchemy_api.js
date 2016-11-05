b3aa19d7e9bae216e3a06de2c9adf38a42e3cf3f
adunham95 <adunham95@gmail.com>
2016-11-04 23:44:20 -0400

    working API call

--- src/js/alchemy_api.js
+++ src/js/alchemy_api.js
@@ -10,13 +10,18 @@ var timeFrame = "&start=now-7d&end=now&count=1";
 var search = "q.enriched.url.entities.entity=|text=";
 var type = "type=company|";
 
+// url + "apikey=" + apikey + mode + mode + timeFrame + search + companyName + type
 function callAlchemy(companyName) {
 
     // Simple response handling
-    fetch(url + "apikey=" + apikey + mode + mode + timeFrame + search + companyName + type).then(function(response) {
-        console.log(response);
-        var watsonData = JSON.parse(response);
-        console.log(watsonData.result)
+    fetch(url + "apikey=" + apikey + mode + mode + timeFrame + search + companyName + type)
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+    }).then(function (j) {
+        console.log(j.status);
+        console.log(j);
+        document.getElementById("status").innerText = j.status;
     }).catch(function(err) {
         // Error :(
         console.log(err)

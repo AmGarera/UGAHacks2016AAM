3906d86776c5fbafc6caab70ae44e0b65ac05abc
adunham95 <adunham95@gmail.com>
2016-11-04 22:27:07 -0400

    Added Api key and basic call

--- src/js/alchemy_api.js
+++ src/js/alchemy_api.js
@@ -1,3 +1,19 @@
 /**
  * Created by apdun on 11/4/2016.
  */
+
+//https://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=b6d15d85a0a1b7f76fcf63b53298a8568eefdf89
+var url = "https://gateway-a.watsonplatform.net/calls/data/GetNews?";
+var apikey =  "b6d15d85a0a1b7f76fcf63b53298a8568eefdf89";
+var mode = "&outputMode=json";
+
+function callAlchemy(companyName) {
+
+    // Simple response handling
+    fetch(url + "apikey=" + apikey + mode).then(function(response) {
+
+    }).catch(function(err) {
+        // Error :(
+    });
+
+}

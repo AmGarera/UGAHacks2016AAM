deda8b468a3e892510eec9eaa4e98048171ddbc6
adunham95 <adunham95@gmail.com>
2016-11-05 20:23:50 -0400

    added add function

--- src/js/dexie_api.js
+++ src/js/dexie_api.js
@@ -1,11 +1,32 @@
 /**
  * Created by apdun on 11/5/2016.
  */
-function saveCompanyList() {
+function saveCompanyList(retSymbol, retCompanyName, retRating, retLogo, retValue) {
+
+    var storeSymbol = retSymbol;
+    var storeCompanyName = retCompanyName;
+    var storeRating = retRating;
+    var storeLogo = retLogo;
+    var storeValue = retValue;
+
+    console.log("storeSymbol: " + storeSymbol);
+    console.log("storeCompanyName: " + storeCompanyName);
+    console.log("storeRating: " + storeRating);
+    console.log("storeLogo: " + storeLogo);
+    console.log("storeValue: " + storeValue);
+
     var db = new Dexie("MyDatabase");
     db.version(1).stores({
-        friends: "++id, campanyName, age, *tags",
-        gameSessions: "id, score"
+        companyList: "symbol, companyName, rating, logo, value"
     });
 
+
+    db.companyList.add(
+        {
+            symbol: storeSymbol,
+            companyName: storeCompanyName,
+            rating: storeRating,
+            logo: storeLogo,
+            value: storeValue
+        });
 }

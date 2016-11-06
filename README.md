fe19dc14d1d7cf914f6697b60123b0b1d7dc0b4f
Michael Mazzone <mazzone.deca@gmail.com>
2016-11-06 06:39:54 +0000

    Fixed chart issue, fixed spaghetti code.

--- README.md
+++ README.md
@@ -3,4 +3,14 @@
   
 ## Stock Sentiment Rating  
 #### Track your favorite publicly traded companies  
-Add companies to your list.
+* Add companies to your list.  
+* View important indicators, such as sentiment rating, stock price, and total media mentions.  
+* Steer clear of companies with strongly negative sentiment.  
+    
+#### Couldn't have done it without:  
+* [Materialize.css](http://materializecss.com/)  
+* [jQuery](https://jquery.com/)  
+* [Dexie](dexie.org)  
+* [IBM Alchemy](https://www.ibm.com/watson/developercloud/alchemy-language.html)  
+* [Quandl](https://www.quandl.com)  
+* [Chart.js](www.chartjs.org)  
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,80 +1,76 @@
-//Side nav enabled and configured
-$('.button-collapse').sideNav({
-      menuWidth: 240,
-      edge: 'right',
-      closeOnClick: true,
-      draggable: true
-    }
-);
-
 //Collapsible company section enabled
-$(document).ready(function(){
+$(document).ready(function() {
+    //Side nav enabled and configured
+    $('.button-collapse').sideNav({
+        menuWidth: 240,
+        edge: 'right',
+        closeOnClick: true,
+        draggable: true
+    });
     $('.collapsible').collapsible();
     $('.button-collapse').sideNav({
-      menuWidth: 240,
-      edge: 'right',
-      closeOnClick: true,
-      draggable: true
-    }
-);
-});
-
-//Chart.js setup
-var ctx = document.getElementById("pChartsCanvas");
-var pChartsCanvas = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+        menuWidth: 240,
+        edge: 'right',
+        closeOnClick: true,
+        draggable: true
+    });
+    //Chart.js setup
+    var ctx = document.getElementById("pChartsCanvas");
+    var pChartsCanvas = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
-});
+    });
 
-//Add companies to list and remove them
-$('.addCompany').click(function(e) {
-    e.preventDefault();
-    
-    var name = localStorage.getItem("cName");
-    var symbol = localStorage.getItem("symbol");
-    var rating = localStorage.getItem("rating");
-    
-    saveCompanyList(symbol, rating, name)
+    //Add companies to list and remove them
+    $('.addCompany').click(function(e) {
+        e.preventDefault();
 
-    $(".collapsible-body").append(
-        '<div class="chip">' + returnedData() + '</div>');
-});
+        var name = localStorage.getItem("cName");
+        var symbol = localStorage.getItem("symbol");
+        var rating = localStorage.getItem("rating");
 
-// Remove parent of 'remove' link when link is clicked.
-$('.chip').on('click', '.chip', function(e) {
-    e.preventDefault();
+        saveCompanyList(symbol, rating, name)
 
-    $(this).parent().remove();
+        $(".collapsible-body").append(
+            '<div class="chip">' + returnedData() + '</div>');
+    });
+
+    // Remove parent of 'remove' link when link is clicked.
+    $('.chip').on('click', '.chip', function(e) {
+        e.preventDefault();
+
+        $(this).parent().remove();
+    });
 });

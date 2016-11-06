c35bbf0d469ec85abfb72e79664cbe36583b7ca8
Michael Mazzone <mazzone.deca@gmail.com>
2016-11-06 00:25:04 +0000

    merge conflicts

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,13 @@
-
 $('searchIcon').addClass('active');
 
-//Side nav enabled and configured
-$('.button-collapse').sideNav({
-      menuWidth: 240, 
-      edge: 'right',
-      closeOnClick: true, 
-      draggable: true 
-    }
-);
-
-//Collapsible company section enabled
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-    $('.button-collapse').sideNav({
-      menuWidth: 240, 
-      edge: 'right',
-      closeOnClick: true, 
-      draggable: true 
-    }
-);
-});
-
-//Chart.js setup
-var ctx = document.getElementById("pChartsCanvas").getContext("2d");
-var posnegChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+var searchedCompany = [];
+var globalSearchedCompanies = top.searchedCompany;
 
 function getCompanyName() {
 
-
     if(event.keyCode == 13) {
         $("#companyLogoImg").attr('src', 'https://logo.clearbit.com/');
-        var input = document.getElementById("search").value;
-        $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + input + ".com");
+        searchedCompany.push(document.getElementById("search").value);
+        $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + searchedCompany[searchedCompany.length - 1] + ".com");
     }
 }
-
---
+++ src/js/profile.js
@@ -0,0 +1,64 @@
+//Side nav enabled and configured
+$('.button-collapse').sideNav({
+      menuWidth: 240, 
+      edge: 'right',
+      closeOnClick: true, 
+      draggable: true 
+    }
+);
+
+//Collapsible company section enabled
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+    $('.button-collapse').sideNav({
+      menuWidth: 240, 
+      edge: 'right',
+      closeOnClick: true, 
+      draggable: true 
+    }
+);
+});
+
+//Chart.js setup
+var ctx = document.getElementById("pChartsCanvas").getContext("2d");
+var posnegChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+
+/* SENTIMENT CALLS START HERE */
+console.log("globalCompany: " + globalCompany);
+

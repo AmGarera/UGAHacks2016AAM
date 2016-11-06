3d9cdffd2069665938b4bc8da1f240ea587226b7
amgmetallica <gareraanthony@gmail.com>
2016-11-06 00:50:18 -0400

    Merge remote-tracking branch 'origin/master' into front-end
    # Conflicts:
    #	src/index.html
    #	src/js/main.js

---
+++ dist/js/setiment.js
@@ -0,0 +1,90 @@
+/**
+ * Created by apdun on 11/5/2016.
+ */
+//Makes the sentiment series array gloabl
+var sentiemntSeries = [];
+var sentiementSeries = [];
+
+$( document ).ready(function() {
+    $('#posnegChart').hide();
+});
+
+function calcSetiment() {
+    document.getElementById('welcomeDiv').style.display = "block";
+    var positive = 0;
+    var negative = 0;
+    var average = 0;
+    console.log("Inside calcSentiment");
+    console.log(allScores);
+
+    // Loops through the array
+    for (var i = 0; i < allScores.length; i++)
+    {
+
+        // Determins if its positive, negative, or nutral
+        console.log(allScores[i]);
+        if (allScores[i] > 0) {
+            console.log("Positive " + allScores[i]);
+            positive = positive + allScores[i];
+            average = average + allScores[i];
+        }
+        else if (allScores[i] == 0) {
+            console.log("Neutral " + allScores[i]);
+        }
+        else {
+            console.log("Negative " + allScores[i]);
+            negative = negative + allScores[i];
+            average = average + allScores[i];
+        }
+
+    }
+
+    console.log("positive" + positive);
+    console.log("negative" + negative);
+    console.log("average" + average);
+
+    var total = positive + (negative*-1);
+
+
+    // Pushed the positive, negative and average values into the array
+    sentiemntSeries.push((positive + (negative*-1))/(negative*-1), ((negative*-1) + positive)/positive) ;
+
+
+    // Displays if its positive, negative or neutral
+    if (average > 0) {
+        document.getElementById("sentiment").innerText = "Positive sentiment!";
+        $('#posnegChart').show();
+    }
+    else if (average == 0) {
+        document.getElementById("sentiment").innerText = "Neutral sentiment!";
+        $('#posnegChart').hide();
+    }
+    else {
+        document.getElementById("sentiment").innerText = "Negative sentiment!";
+        $('#posnegChart').show();
+    }
+
+    console.log(sentiemntSeries);
+
+
+    var ctx = document.getElementById("posnegChart").getContext('2d');
+    var posnegChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["Positive Sentiment", "Negative Sentiment"],
+            datasets: [{
+                backgroundColor: [
+                    "#00e676",
+                    "#f44336",
+                    "#95a5a6",
+                    "#9b59b6",
+                    "#f1c40f",
+                    "#e74c3c",
+                    "#34495e"
+                ],
+                data: sentiemntSeries
+            }]
+        }
+    });
+
+}

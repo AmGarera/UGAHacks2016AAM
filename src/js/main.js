727681f8c52c4b68c6a5d4bfb0d2c189b49e4d00
adunham95 <adunham95@gmail.com>
2016-11-06 01:21:45 -0500

    code clean up

--- src/js/main.js
+++ src/js/main.js
@@ -71,14 +71,14 @@ var posnegChart = new Chart(ctx, {
 $('searchIcon').addClass('active');
 
 var searchedCompany = [];
-var globalSearchedCompanies = window.searchedCompany;
+var globalSearchedCompanies = searchedCompany;
 
 
 function getCompanyName() {
 
     if (event.keyCode == 13) {
         $("#companyLogoImg").attr('src', 'https://logo.clearbit.com/');
-        searchedCompany.push(document.getElementById("search").value);
+        // searchedCompany.push(document.getElementById("search").value);
         $("#companyLogoImg").attr('src', $('#companyLogoImg').attr('src') + searchedCompany[searchedCompany.length - 1] + ".com");
 
 
@@ -90,16 +90,16 @@ function getCompanyName() {
 }
 
 
-/* SENTIMENT CALLS START HERE */
-//STOCK PROVIDES THESE
-window.symbol;
-window.cValue;
-
-//ALCH PROVIDES THESE
-window.cName;
-window.sRating;
-window.cLogo;
-
-window.cName = globalSearchedCompanies[globalSearchedCompanies.length - 1];
-window.sRating = sentimentScore;
-window.cLogo = "https://logo.clearbit.com/" + cName + ".com";
+// /* SENTIMENT CALLS START HERE */
+// //STOCK PROVIDES THESE
+// window.symbol;
+// window.cValue;
+//
+// //ALCH PROVIDES THESE
+// window.cName;
+// window.sRating;
+// window.cLogo;
+//
+// window.cName = globalSearchedCompanies[globalSearchedCompanies.length - 1];
+// window.sRating = sentimentScore;
+// window.cLogo = "https://logo.clearbit.com/" + cName + ".com";
